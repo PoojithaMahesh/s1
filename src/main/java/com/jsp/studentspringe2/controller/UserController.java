db42b2e6a891fe655e217bcfab3f4d6b3f3ae059
unknown <poojithadarshan2131@gmail.com>
2023-11-08 20:12:10 +0530

    final

--- src/main/java/com/jsp/studentspringe2/controller/UserController.java
+++ src/main/java/com/jsp/studentspringe2/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.jsp.studentspringe2.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jsp.studentspringe2.dao.UserDao;
@@ -13,8 +20,32 @@ public class UserController {
     @Autowired
 	private UserDao userDao;
 	
-	@RequestMapping("/save")
+	@PostMapping("/save")
 	public User saveUser(@RequestBody User user) {
 		return userDao.saveUser(user);
 	}
+	@GetMapping("/find")
+	public User findUserById(@RequestParam int userId) {
+		return userDao.findUserById(userId);
+	}
+	
+	@DeleteMapping("/delete")
+	public User deleteUserById(@RequestParam int userId) {
+		return userDao.deleteUserById(userId);
+	}
+	@GetMapping("/findall")
+	public List<User> findAll(){
+     return userDao.getAllUser();
+	}
+	
+	@PutMapping("/update")
+	public User updateUser(@RequestParam int userId,@RequestBody User user) {
+		return userDao.updateUser(userId,user);
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/jsp/studentspringe2/dao/UserDao.java
+++ src/main/java/com/jsp/studentspringe2/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.jsp.studentspringe2.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +18,48 @@ public class UserDao {
 	
 	public User saveUser(User user) {
 		return repo.save(user);	
+	}
+
+	public User findUserById(int id) {
+		Optional<User> optional=repo.findById(id);
+		
+		if(optional.isPresent()) {
+//			id is present
+			User user=optional.get();
+			return user;
+		}else {
+//			id is not present
+			return null;
+		}
+	}
+
+	public User deleteUserById(int userId) {
+
+      Optional<User> optional=repo.findById(userId);
+		if(optional.isPresent()) {
+//			id is present and now i can delete this user
+			User user=optional.get();
+			repo.delete(user);
+			return user;
+		}
+		return null;
+	}
+
+	public List<User> getAllUser() {
+	
+		return repo.findAll();
+	}
+
+	public User updateUser(int userId, User user) {
+//	 how to update
+		Optional<User> optional=repo.findById(userId);
+		if(optional.isPresent()) {
+//			id is present then i can update the data
+			user.setId(userId);
+			return repo.save(user);
+		}
+		
+		return null;
 	}
 
 	
